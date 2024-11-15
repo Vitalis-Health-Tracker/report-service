9f870e259d4d965849c8b80ac3acb340380ba4d5
silvercen <abelkm123@gmail.com>
2024-11-15 11:36:24 +0530

    Added endpoints so frontend can gather report data

--- src/main/java/com/example/report_service/controller/ReportController.java
+++ src/main/java/com/example/report_service/controller/ReportController.java
@@ -1,7 +1,10 @@
 package com.example.report_service.controller;
 
+import com.example.report_service.model.Report;
 import com.example.report_service.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,15 @@ public class ReportController {
     @Autowired
     private ReportService reportService;
 
+    @GetMapping("/get-report/{userId}")
+    public ResponseEntity<Report> getReport(String userId){
+        try
+        {
+            return ResponseEntity.ok(reportService.getReport(userId));
+        }
+        catch (RuntimeException e)
+        {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
 }
--- src/main/java/com/example/report_service/service/ReportService.java
+++ src/main/java/com/example/report_service/service/ReportService.java
@@ -183,4 +183,18 @@ public class ReportService {
             generateReport(userId, startDate, endDate);
         }
     }
+
+    public Report getReport(String userId) {
+        LocalDateTime startDate = LocalDateTime.now().minusDays(7);
+        LocalDateTime endDate = LocalDateTime.now().minusDays(0);
+        Mono<Report> report = getReportByDate(LocalDateTime.now(), userId);
+        if(report.block() == null)
+        {
+            throw new RuntimeException("Report not found");
+        }
+        else
+        {
+            return report.block();
+        }
+    }
 }
